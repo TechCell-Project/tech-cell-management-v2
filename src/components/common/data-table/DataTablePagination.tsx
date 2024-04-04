7b20f22c4f06e3170dbca7d281217a488c3e732f
bou2201 <lamchuc123456@gmail.com>
2024-04-04 23:56:55 +0700

    feat(components): :sparkles: `data-table` - add record limit pagination

--- src/components/common/data-table/DataTablePagination.tsx
+++ src/components/common/data-table/DataTablePagination.tsx
@@ -1,7 +1,8 @@
 import { Button } from '@/components/ui';
-import { MoveLeft, MoveRight } from 'lucide-react';
+import { ChevronsUpDown, MoveLeft, MoveRight } from 'lucide-react';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { memo, useCallback } from 'react';
+import { memo, useCallback, useMemo } from 'react';
+import { DropdownDisplay, DropdownDisplayItemProps } from '../display';
 
 type DataTablePaginationProps = {
   page: number;
@@ -25,33 +26,72 @@ const DataTablePagination = memo(({ page, limit, hasNextPage }: DataTablePaginat
   );
 
   const changePage = useCallback(
-    (page: number) => {
-      router.replace(pathname + '?' + createQueryString('page', `${page}`));
+    (param: string, page: number) => {
+      router.replace(pathname + '?' + createQueryString(param, `${page}`));
     },
     [createQueryString, pathname, router],
   );
 
+  const limitsRecord: DropdownDisplayItemProps[] = useMemo(() => {
+    return [
+      {
+        content: '5',
+        onClick: () => changePage('limit', 5),
+      },
+      {
+        content: '10',
+        onClick: () => changePage('limit', 10),
+      },
+      {
+        content: '20',
+        onClick: () => changePage('limit', 20),
+      },
+      {
+        content: '30',
+        onClick: () => changePage('limit', 30),
+      },
+      {
+        content: '40',
+        onClick: () => changePage('limit', 40),
+      },
+    ];
+  }, [changePage]);
+
   return (
     <div className="flex items-center justify-end space-x-2 py-4">
+      <div className="flex items-center gap-2 mr-6">
+        <p className="text-[13px]">Bản ghi:</p>
+        <DropdownDisplay
+          trigger={
+            <Button variant="outline" className="h-9 px-3 flex items-center gap-4">
+              <span className='text-[13px]'>{limit}</span>
+              <span className="sr-only">Open menu</span>
+              <ChevronsUpDown className="h-3 w-3" />
+            </Button>
+          }
+          label="Bản ghi"
+          items={limitsRecord}
+        />
+      </div>
       <Button
         variant="outline"
         size="sm"
-        onClick={() => changePage(page - 1)}
+        onClick={() => changePage('page', page - 1)}
         disabled={+page <= 1}
-        className='flex items-center gap-2 text-[12px]'
+        className="flex items-center gap-2 text-[12px]"
       >
-        <MoveLeft className='w-[15px]' />
+        <MoveLeft className="w-[10px]" />
         Trang trước
       </Button>
       <Button
         variant="outline"
         size="sm"
-        onClick={() => changePage(page + 1)}
+        onClick={() => changePage('page', page + 1)}
         disabled={!hasNextPage}
-        className='flex items-center gap-2 text-[12px]'
+        className="flex items-center gap-2 text-[12px]"
       >
         Trang kế
-        <MoveRight className='w-[15px]'/>
+        <MoveRight className="w-[10px]" />
       </Button>
     </div>
   );
