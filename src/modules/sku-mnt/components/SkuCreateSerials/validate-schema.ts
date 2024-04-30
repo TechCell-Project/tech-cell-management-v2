3382a981d0dced2dd557ffa31d8b9164c610eb93
bou2201 <lamchuc123456@gmail.com>
2024-04-30 23:03:12 +0700

    refactor(modules): :recycle: refactor create serial number form

--- src/modules/sku-mnt/components/SkuCreateSerials/validate-schema.ts
+++ src/modules/sku-mnt/components/SkuCreateSerials/validate-schema.ts
@@ -3,10 +3,23 @@ import * as yup from 'yup';
 
 export const createSerialValidateSchema: yup.ObjectSchema<any> = yup.object({
   serialNumbers: yup.array(
-    yup
-      .string()
-      .min(12, 'Ít nhất 12 ký tự')
-      .max(24, 'Nhiều nhất 24 ký tự')
-      .required(FIELD_REQUIRED),
+    yup.object({
+      serial: yup
+        .string()
+        .min(12, 'Ít nhất 12 ký tự')
+        .max(24, 'Nhiều nhất 24 ký tự')
+        .required(FIELD_REQUIRED)
+        .test('unique-serials', 'Số serial không được trùng lặp', function (value) {
+          if (!value) return true;
+          const serials = new Set();
+          for (const item of value) {
+            if (serials.has(item)) {
+              return false;
+            }
+            serials.add(item);
+          }
+          return true;
+        }),
+    }),
   ),
 });
--- src/modules/sku-mnt/models/sku-create-serial-num.ts
+++ src/modules/sku-mnt/models/sku-create-serial-num.ts
@@ -1,5 +1,11 @@
 import { AddSerialNumberDto } from '@techcell/node-sdk';
 
+type SerialObj = { serial: string };
+
+export class SkuCreateSerialObj {
+  serialNumbers: SerialObj[] = [];
+}
+
 export class SkuCreateSerialNum implements AddSerialNumberDto {
-  serialNumbers: string[] = [''];
+  serialNumbers: string[] = [];
 }
